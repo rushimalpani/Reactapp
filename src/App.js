2e2a2382d70926ad2267a606cbc3deca2cae0494
Rushi <rushi7777@gmail.com>
2023-11-26 18:57:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+Assognment No. 5        </p>
+        <a
+          className="App-link"
+          href="https://www.vit.edu/"
+          // target="_blank"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          VIT Pune
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
